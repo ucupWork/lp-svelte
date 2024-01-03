9a754bf24912ba0675fa9f1d4035b274d0ea9154
ucupWork <work.ysetiawan@gmail.com>
2024-01-03 17:03:33 +0700

    adding company about

---
+++ src/routes/tentang-kami/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Navbar from "../../lib/components/navbar.svelte";
+    import Footer from "../../lib/components/footer.svelte";
+</script>
+<Navbar/>
+<div class="p-10">
+    <div class="p-3">
+    <h1 class="text-5xl font-extrabold text-neutral-600 pb-3">
+        MUSCLE FIRST
+    </h1>
+    <p >
+        ( PT Unggulan Bugar Indonesia ) Muscle First adalah pelopor suplemen fitnes terbaik dari Indonesia dengan banyak varian rasa anti-bosan yang telah bersertifikat <b> 
+        Labdoor, GMP, HACCP, ISO, dan Halal BPOM
+        </b>
+        . Muscle First didirikan pada tahun 2017 dan telah mengalami peningkatan sebesar 17.000% sejak diluncurkan dalam 2 tahun, sehingga produk Muscle First memimpin penjualan suplemen terbaik di pasar Indonesia dan telah bekerjasama dengan lebih dari 100+ agen di Indonesia.
+        <br><br>
+        Muscle First membantu pria dan wanita dalam recovery dan memelihara massa otot, membangun tubuh ideal seperti menurunkan atau menambah berat badan, memenuhi nutrisi harian tubuh, meningkatkan stamina, dan meningkatkan fokus. Visi Muscle First adalah Menjadi brand suplemen fitness nomor satu di Indonesia dengan standar kebugaran Internasional.
+    </p>
+    </div>
+    <a href="/products">
+        <button class="p-4 sm:p-2 bg-gray-800 rounded-full text-white hover:bg-gray-600 hover:shadow-2xl md:text-sm sm:text-sm">Lihat Produk Kami</button>
+    </a>
+</div>
+<Footer/>
